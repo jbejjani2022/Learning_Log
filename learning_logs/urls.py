49df74695662575d141dee0608d0ec9fb39ad475
jbejjani2022 <jpbejjani@hotmail.com>
2020-06-28 19:13:53 -0400

    Created functionality to edit an entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
 
     # Page for adding new entry
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Page for editing entry
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 ]
