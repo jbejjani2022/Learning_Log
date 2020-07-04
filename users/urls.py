02652facb61b575f4af19e23b8a96b9129a3a496
jbejjani2022 <jpbejjani@hotmail.com>
2020-07-03 23:23:56 -0400

    Create registration url, view func, and html template

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     # Login page
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
